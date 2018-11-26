aabf5ba4a83b5509e568fe154636b5a9ae9a79d8
Bourke, David <davidbourke@quickenloans.com>
2018-11-26 15:55:28 -0500

    first commit

---
+++ Day28/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Day28
+{
+
+
+
+    static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        for (int NItr = 0; NItr < N; NItr++)
+        {
+            string[] firstNameEmailID = Console.ReadLine().Split(' ');
+
+            string firstName = firstNameEmailID[0];
+
+            string emailID = firstNameEmailID[1];
+        }
+    }
+}
